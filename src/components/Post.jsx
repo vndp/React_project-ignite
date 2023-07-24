bb87edab086e343924c073b8ee8b6c8650fa3191
Vit <vitoriaperez6@gmail.com>
2023-07-24 12:21:04 -0300

    Update_05

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -3,6 +3,7 @@ import styles from './Post.module.css'
 export function Post() {
     return (
         <article className={styles.post}>
+
             <header>
                 <div className={styles.author}>
                     <img className={styles.avatar} src="http://github.com/vndp.png" />
@@ -11,10 +12,29 @@ export function Post() {
                         <span>Web develop</span>
                     </div>
                 </div>
-                <time dateTime="">Publicado há </time>
-
-
+                <time title="11 de Maio ás 08:13h" dateTime="24-07-23 10:34:00">Publicado há 1h</time>
             </header>
+            <div className={styles.content}>
+                <p>Fala galeraa 👋</p>
+
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+
+                <p> <a href="">jane.design/doctorcare</a></p>
+
+                <p>
+                    <a href="">#novoprojeto</a>{' '}
+                    <a href=""> #nlw </a>{' '}
+                    <a href="">#rocketseat</a>{' '}
+                </p>
+            </div>
+
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback!</strong>
+                <textarea
+                    placeholder="Deixe um comentário :)"
+                />
+                <button type="submit"></button>
+            </form>
         </article>
     )
 }
